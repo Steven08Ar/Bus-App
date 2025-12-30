324cbedbbc431b6f1e8f7ba2fe8684e02caca3ed
Santiago Arias <steven08santi@gmail.com>
2025-12-30 17:19:49 -0500

    Create EstudiantesService

--- backend/server/src/app.module.ts
+++ backend/server/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EstudiantesService } from './estudiantes/estudiantes.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, EstudiantesService],
 })
 export class AppModule {}
